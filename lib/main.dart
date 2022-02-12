783cc8749c49265992205e863cfb0f23ddd6eb67
Vidal Cardona <vrosas832@gmail.com>
2022-02-12 00:36:40 -0600

    feat(Firebase): extensions was fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'platzi_trips.dart';
 import 'platzi_trips_cupertino.dart';
+import 'package:platzi_tripss_app/User/bloc/bloc_user.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,14 +17,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]); // Para la barrita en android. Entrar en pantalla completa
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.blue,
-      ),
+    /*La composición nos dice: Cuál es el bloc que quieres exponer
+    * y a que widget hijo se lo quieres exponer*/
 
-      home: PlatziTripsCupertino()
+    /*De esta manera, todos los métodos que yo ponga en UserBloc
+    * podrán ser accedidos por todos los hijos de MaterialApp*/
+    return BlocProvider(
+        child: MaterialApp(
+            title: 'Flutter Demo',
+            home: PlatziTripsCupertino()
+        ),
+        bloc: UserBloc()
     );
   }
 }
